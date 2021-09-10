d2e153287d5e7cd520f09836fff7fe0b9b4e56bd
Alexander Grube <alexp.grube@googlemail.com>
2021-09-10 15:43:27 +0200

    test heroku db

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"log"
 	"os"
 	"time"
 
 	model "github.com/spctr-cc/backend-bugtrack/model"
+
+	"database/sql"
 
 	"github.com/gofiber/fiber/v2"
 	_ "github.com/lib/pq"
@@ -15,6 +18,10 @@ var (
 )
 
 func main() {
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
@@ -27,18 +34,22 @@ func main() {
 
 	account := model.Account{
 		Firstname: "John",
-		Lastname: "Doe",
-		Email: "john.doe@example.com",
-		Password: "johnIsADoe",
-		Mobile: "555-12345678",
-		Street: "John Doe Street",
-		Zipcode: "12345",
-		City: "John Doe City",
-		Country: "Canada",
+		Lastname:  "Doe",
+		Email:     "john.doe@example.com",
+		Password:  "johnIsADoe",
+		Mobile:    "555-12345678",
+		Street:    "John Doe Street",
+		Zipcode:   "12345",
+		City:      "John Doe City",
+		Country:   "Canada",
 	}
 
 	app.Post("/account", func(c *fiber.Ctx) error {
 		return c.JSON(account)
+	})
+
+	app.Get("/db", func(c *fiber.Ctx) error {
+		return c.JSON(db.Stats().InUse)
 	})
 
 	app.Listen(PORT)
