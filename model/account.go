49e6b8d038e601ffd6e3c47ae9eb8fc4d0224e9f
Alexander Grube <alexp.grube@googlemail.com>
2021-09-11 15:02:45 +0200

    test json validation

--- model/account.go
+++ model/account.go
@@ -9,7 +9,7 @@ type Account struct {
 	Email     string `json:"email" validate:"required,email" gorm:"typevarchar(100);unique_index"`
 	Password  string `json:"password" validate:"required"`
 	Mobile    string `json:"mobile" validate:"required"`
-	Street    string `json:"street" validate:"required, min=2,max=100"`
+	Street    string `json:"street" validate:"required,min=2,max=100"`
 	Zipcode   string `json:"zipcode" validate:"required"`
 	City      string `json:"city" validate:"required"`
 	Country   string `json:"country" validate:"required"`
