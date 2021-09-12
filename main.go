658289be34dfe1dcb2b38fe0524c90f6d40d1810
Alexander Grube <alexp.grube@googlemail.com>
2021-09-12 10:28:56 +0200

    test logger middleware

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 	"gorm.io/gorm"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 var (
@@ -33,6 +34,8 @@ func main() {
 	fmt.Println("Database Migrated")
 
 	app := fiber.New()
+
+	app.Use(logger.New())
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		ticket := model.Ticket{
