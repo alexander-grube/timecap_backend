59ee6338598c6f661e09005d4063f8839ec203f3
Alexander Grube <4531342-alexander.grube@users.noreply.gitlab.com>
2021-09-13 20:34:36 +0200

    fix model

--- model/account.go
+++ model/account.go
@@ -9,15 +9,15 @@ import (
 
 type Account struct {
 	gorm.Model
-	Firstname string `json:"firstname" validate:"required,min=2,max=100"`
-	Lastname  string `json:"lastname" validate:"required,min=2,max=100"`
-	Email     string `json:"email" validate:"required,email" gorm:"typevarchar(100);uniqueIndex"`
-	Password  string `json:"password" validate:"required"`
-	Mobile    string `json:"mobile" validate:"required"`
-	Street    string `json:"street" validate:"required,min=2,max=100"`
-	Zipcode   string `json:"zipcode" validate:"required"`
-	City      string `json:"city" validate:"required"`
-	Country   string `json:"country" validate:"required"`
+	Firstname string
+	Lastname  string
+	Email     string `gorm:"typevarchar(100);uniqueIndex"`
+	Password  string
+	Mobile    string
+	Street    string
+	Zipcode   string
+	City      string
+	Country   string
 }
 
 func (a *Account) HashPassword(plain string) (string, error) {
