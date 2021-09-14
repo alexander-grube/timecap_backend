158f2b5e0f4e24516ddcfbbd299314345293e846
Alexander Grube <alexp.grube@googlemail.com>
2021-09-14 15:39:48 +0200

    test jwt

--- handler/routes.go
+++ handler/routes.go
@@ -17,7 +17,6 @@ func (h *Handler) Register(r *fiber.App) {
 	guestUsers := v1.Group("/users")
 	guestUsers.Post("/login", h.Login)
 	guestUsers.Post("", h.SignUp)
-	account := v1.Group("/account")
-	account.Use(jwtMiddleware)
+	account := v1.Group("/account", jwtMiddleware)
 	account.Get("", h.CurrentAccount)
 }
