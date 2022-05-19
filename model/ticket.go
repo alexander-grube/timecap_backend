ad3937c1b49d26a8354df7749cd10ceb2250b259
Alexander Grube <alexander.grube@cancom.de>
2022-05-19 14:59:52 +0200

    account added to ticket

--- model/ticket.go
+++ model/ticket.go
@@ -34,5 +34,5 @@ type Ticket struct {
 	Priority    TicketPriority
 	Type        TicketType
 	Status      TicketStatus
-	Account     Account `gorm:"foreignkey:ID"`
+	Account     Account `gorm:"foreignkey:AccountID"`
 }
